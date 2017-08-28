0d88bbdca5275cef8d46778fbbb912ab924601f8
unknown <C:\Users\Ruslan\AppData\Roaming\The Bat!>
2017-08-28 18:13:08 +0300

    Add test 123

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+#test 123
+
 class ProductImageInline(admin.TabularInline):		#ств клас ProductImageInline щоб добавити його в адмінці до розділу продукти(тобто коли зайдем на конкретний продукт, то в низ убудуть і фотки з моделі ProductImage)
 	model = ProductImage			
 	extra = 0
